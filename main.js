eb0ebd3675e1265ce849002de23c1eafc72b1c33
Victor Ilchuk <ilchuk710@gmail.com>
2019-05-08 22:57:43 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// window.onload = init;
+
+// function init() {
+//     const keys = document.getElementsByClassName('pianoKey');
+//     for (let i = 0; i < keys.length; i++) {
+//         keys[i].onclick = playNote;
+//     }
+// };
+
+// function playNote() {
+
+// }
+
+
+
+// // const keyA = document.createElement("div");
+// // keyA.id = "myDiv";
+// // keyA.innerHTML = "Some text with <b>bold text</b>";
+// console.log('kbkhb');
+
+const newElem = document.createElement("div");
+newElem.id = "my-div";
+newElem.innerHTML = "Some text with <b>bold text</b>";
+
